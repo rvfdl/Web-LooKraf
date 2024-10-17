fea9b950ebce65c761fb7ed57ea48b5002c90860
rvfdl <pratamadani563@gmail.com>
2024-10-17 09:59:13 +0700

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = () => (
       {/* Kolom Kanan - Judul dan Deskripsi */}
       <div className="md:w-1/2 md:pl-8 text-center md:text-left">
         <h2 className="text-0xl font-bold mb-4 underline text-white">LOOKRAF</h2>
-        <h2 className="font-bold mb-4 text-5xl text-white">Jasa Ngontol Logo Murah, Cepat dan Berkualitas</h2>
+        <h2 className="font-bold mb-4 text-5xl text-white">Jasa Desain Logo Murah, Cepat dan Berkualitas</h2>
       </div>
     </div>
   </section>
